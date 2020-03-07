f23ff957bb00392412c3c64e53a8026b87b38c0f
Ivan Leskin <leskin.in@arenadata.io>
2020-03-07 11:33:20 +0300

    WIP

--- src/kem/ntru/pqclean_ntruhps4096821_clean/api.h
+++ src/kem/ntru/pqclean_ntruhps4096821_clean/api.h
@@ -12,6 +12,12 @@
 
 int PQCLEAN_NTRUHPS4096821_CLEAN_crypto_kem_keypair(uint8_t *pk, uint8_t *sk);
 
+/*@
+    requires \valid_read (c + (0..1230));
+    requires \valid_read (pk + (0..1230));
+    requires \valid (k + (0..32));
+    ensures \initialized (k + (0..32));
+ */
 int PQCLEAN_NTRUHPS4096821_CLEAN_crypto_kem_enc(uint8_t *c, uint8_t *k, const uint8_t *pk);
 
 int PQCLEAN_NTRUHPS4096821_CLEAN_crypto_kem_dec(uint8_t *k, const uint8_t *c, const uint8_t *sk);
